9bac78ead8afd055ed58868dd05fe9658e2d9ba6
arusakov <a_s_rusakov@mail.ru>
2016-10-05 01:33:25 +0300

    Protocol class

--- client/one.ts
+++ client/one.ts
@@ -1,27 +1,33 @@
 import { Connection } from "../src/node-tarantool";
-import { getPrefix } from "../src/protocol";
 import {
     TCodePing,
     TKeyCode,
+    TKeyIndexId,
+    TKeyIterator,
+    TKeySpaceId,
     TKeySync,
- } from "../src/tarantool-types";
+} from "../src/tarantool-types";
 
 const con = new Connection({
     port: 3301,
 });
-
 con.connect();
 
 // msgpack map https://github.com/msgpack/msgpack/blob/master/spec.md#map-format-family
 const header = new Buffer([
     0b10000010, // msgpack map with N=2
     TKeyCode, TCodePing, // code => ping
-    TKeySync, 1, // sync => 1
+    TKeySync, 42, // sync => 1
 ]);
 console.log(header);
 
-const packet = [getPrefix(header.length), header];
+const body = new Buffer([
+    0b10000011, // msgpack map with N=1
+    TKeySpaceId, 512, // spaceId => 272
+    TKeyIndexId, 512, // keyId => 512
+    TKeyIterator, 0, // itaretor => all (0)
+]);
 
 setTimeout(() => {
-    con.send(Buffer.concat(packet));
+    con.send(header, body);
 }, 1000);
