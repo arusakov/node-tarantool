f4f73fb70b545fbbc00c4c4034f6daa1375079b1
arusakov <a_s_rusakov@mail.ru>
2016-10-16 02:38:30 +0300

    use >>>

--- src/msgpack/map.ts
+++ src/msgpack/map.ts
@@ -3,7 +3,7 @@
  */
 export function isFixMap(byte: number): boolean {
     /* tslint:disable:no-bitwise */
-    return (byte >> 4) === 0b1000;
+    return (byte >>> 4) === 0b1000;
     /* tslint:enable:no-bitwise */
 }
 
