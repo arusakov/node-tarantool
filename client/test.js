d887eb1395e3cb35d1ccded0260c8941c20c6bf8
arusakov <a_s_rusakov@mail.ru>
2016-10-17 01:57:06 +0300

    next

--- client/test.js
+++ client/test.js
@@ -3,13 +3,14 @@ var conn = new TarantoolConnection({
     port: 3301
 });
 conn.connect()
-    .then(function () {
-        //auth for login, password
-        return conn.auth('test', 'test');
-    }).then(() => {
+    // .then(function () {
+    //     //auth for login, password
+    //     return conn.auth('test', 'test');
+    // })
+    .then(() => {
         // select arguments space_id, index_id, limit, offset, iterator, key
-        return conn.select(512, 0, 1, 0, 'eq', [50]);
+        return conn.select(512, 0, 10, 0, 'all');
     })
     .then((results) => {
-        doSomeThingWithResults(results);
+        console.log(results);
     });
