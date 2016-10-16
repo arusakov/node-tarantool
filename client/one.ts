22d9d7fcc450080b93835b31630c09c17b288787
arusakov <a_s_rusakov@mail.ru>
2016-10-17 01:10:43 +0300

    missing field key

--- client/one.ts
+++ client/one.ts
@@ -1,13 +1,13 @@
 import { Connection } from "../src/node-tarantool";
 import {
-    TCodePing,
+    TCodeSelect,
     TKeyCode,
     TKeyIndexId,
     TKeyIterator,
     TKeyKey,
     TKeyLimit,
     TKeyOffset,
-    TKeySchemaId,
+    // TKeySchemaId,
     TKeySpaceId,
     TKeySync,
 } from "../src/tarantool-types";
@@ -19,21 +19,18 @@ con.connect();
 
 // msgpack map https://github.com/msgpack/msgpack/blob/master/spec.md#map-format-family
 const header = new Buffer([
-    0b10000011, // msgpack map with N=2
-    TKeyCode, TCodePing, // code => ping
-    TKeySync, 42, // sync => 1
-    TKeySchemaId, 0x34, // schemaId => 52
+    0b10000010, // msgpack map with N=2
+    TKeyCode, TCodeSelect, // code => ping
+    TKeySync, 1, // sync => 1
+    // TKeySchemaId, 0x34, // schemaId => 52
 ]);
 console.log(header);
 
 const body = new Buffer([
-    0b10000001, // msgpack map with N=6
+    0b10000010, // msgpack map with N=1
     TKeySpaceId, 0xcd, 0x02, 0x00, // spaceId => 512
-    TKeyIndexId, 0xcd, 0x02, 0x00, // keyId => 512
-    TKeyLimit, 0, // itaretor => all (0)
-    TKeyOffset, 0, // itaretor => all (0)
-    TKeyIterator, 0, // itaretor => all (0)
-    TKeyKey, 0, // itaretor => all (0)
+    TKeyLimit, 10,
+    // TKeyKey, 0,
 ]);
 
 setTimeout(() => {
